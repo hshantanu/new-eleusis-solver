5507dffe35d8c6250dc00b5de3b612c671fc8417
Rahul Raghavan <raghavan_rahul@yahoo.in>
2016-11-14 23:35:08 -0500

    adding map_card_to_property function

--- New_Eleusis.py
+++ New_Eleusis.py
@@ -1,10 +1,66 @@
 import random
 
-def pick_random_card():
-	rank = random.choice( ('A','2','3','4','5','6','7','8','9','T','J','Q','K') )
-	suit = random.choice( ('C','D','H','S') )
+
+# R => Red
+# B => Black
+# C => Club
+# D => Diamond
+# H => Hearts
+# S => Spade
+numeric_characterstic = ['A','2','3','4','5','6','7','8','9','T','J','Q','K']
+suit_characterstic = ['C','S','D','H']
+
+def pick_random_card(card_characterstic_list):
+	if (card_characterstic_list):
+		card_number = card_characterstic_list['numeric_characterstic']
+		if(len(card_characterstic_list['suit_characterstic'])==1):
+			card_suit = card_characterstic_list['suit_characterstic']
+		else:
+			# case when multiple card characterstic are present for example getting negative card
+			card_suit = card_characterstic_list['suit_characterstic'].split(',')
+		# TODO need to handle removing color
+		# TODO And removing royal card
+
+		numeric_characterstic.remove(card_number) # remove the number
+		suit_characterstic.remove(card_suit) # remove the suit as we dont want a random card with the same suit
+	rank = random.choice( numeric_characterstic )
+	suit = random.choice( suit_characterstic )
+>>>>>>> Stashed changes
 	card = rank + suit
 	return card
 
 
+<<<<<<< Updated upstream
 print pick_random_card()
+=======
+
+def initialize_negative_characteristic_list(card_characterstic_list):
+	# mapping of negative character
+	negative_characterstic_list = {'Black':'Red','Red':'Black', 'C':'H,D' , 'S' : 'H,D', 'H': 'S,C', 'D': 'S,C'}
+	if (not card_characterstic_list):
+		print('Characterstic list empty')
+		return None
+	characterstic_list = []
+	if (card_characterstic_list['suit_characterstic']) :
+		suit_characterstic_values = negative_characterstic_list[card_characterstic_list['suit_characterstic']].split(',')
+		characterstic_list.append(suit_characterstic_values[0])
+		characterstic_list.append(suit_characterstic_values[1])
+	characterstic_list.append(negative_characterstic_list[card_characterstic_list[2]])
+	return pick_random_card(characterstic_list)
+
+
+def map_card_characteristic_to_property():
+	'''
+		Return a mapping of all the card characterstic to the property
+	'''
+	return {'C1' : 'Red', 'C2':'Black', 'C3': 'D', 'C4':'H', 'C5':'S', 'C6':'C', 'C7':'Even', 'C8':'Odd', 'C9': 'Royal', 'C10':'Not_Royal'}
+
+
+# 0 => number
+# 1 => suit
+# 2 => color
+# sample test
+card_characterstic_list = {'numeric_characterstic': '9', 'suit_characterstic': 'C', 'color_characterstic': 'Black'}
+print(map_card_characteristic_to_property())
+
+
