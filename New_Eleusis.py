7028f9e36ccdf8378c49e9032999fe6c4fa98a92
Shantanu Hirlekar <you@example.com>
2016-11-14 23:08:53 -0500

    This is test message

--- New_Eleusis.py
+++ New_Eleusis.py
@@ -2,10 +2,9 @@ import random
 
 def pick_random_card():
 	rank = random.choice( ('A','2','3','4','5','6','7','8','9','T','J','Q','K') )
-	suit = random.choice( ('c','d','h','s') )
+	suit = random.choice( ('C','D','H','S') )
 	card = rank + suit
 	return card
 
 
-print(pick_random_card())
-print('Blah Blah!')
+print pick_random_card()
