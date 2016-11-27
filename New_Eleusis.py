1b034c4db5972fbc1090d4733970812420a99c7d
vipin <vipin2pillai@gmail.com>
2016-11-26 21:37:52 -0500

    Resolving merges

--- New_Eleusis.py
+++ New_Eleusis.py
@@ -226,7 +226,7 @@ def scientist():
 	# while plays <= 200:
 	# 	#play(<card>)
 	# 	parse_illegal_indices()
-	# 	initalize_characteristic_list()
+	# 	initalize_characteristic_list()ia
 	# 	#TODO Please remove
 	# 	characteristic_list = get_card_characteristics('7H')
 	# 	# update_characteristic_list(characteristic_list)
@@ -573,7 +573,7 @@ def initialize_variable_offset():
 	elif count is 1:
 		curr = board_state[-1]
 		offset_list.append(curr)
-		print "[Current]: ",offset_def scan_and_rank_hypothesis():list
+		print "[Current]: ",offset_list
 	elif count is 0:
 		print 'Board empty'
 	elif count is 2:
