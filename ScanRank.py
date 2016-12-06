965f0225d9c800d2729e94bf47a7d708b48d7af7
Rahul Raghavan <raghavan_rahul@yahoo.in>
2016-12-06 17:55:19 -0500

    Adding scan and rank

--- ScanRank.py
+++ ScanRank.py
@@ -1,3 +1,12 @@
+# Team Name: Guess My Rule! 
+
+# Members: 
+# Vipin Pillai
+# Sailakshmi Pisupati
+# Rahul Raghavan
+# Shantanu Hirlekar
+
+
 import random
 import operator
 import itertools
@@ -5,91 +14,171 @@ from collections import OrderedDict
 from itertools import combinations
 import time
 from NewEleusisHelper import *
-from New_Eleusis import find_numeric_characteristic_relation
-char_dict = {}
+
+char_dict={}
 
 def scan_and_rank_hypothesis(three_length_hypothesis_flag):
+    
     hypothesis_dict = {}
+    orf_flag = True
+    #get legal indices from the the board state
     board_state = parse_board_state()
+
     legal_cards = board_state['legal_cards']
-    print str(legal_cards)
     characteristic_index_list = []
     for i in xrange(0, len(legal_cards)):
         update_characteristic_list(legal_cards[i], i, char_dict)
 
     weighted_property_dict = set_characteristic_weights()
     hypothesis_occurrence_count = {}
+    
     mean = 0.0
     if three_length_hypothesis_flag:
         for i in xrange(2, len(legal_cards)):
-            combined_char_indices_list = [char_dict[i - 2], char_dict[i - 1], char_dict[i]]
+
+            combined_char_indices_list = [char_dict[i-2], char_dict[i-1], char_dict[i]]
+
+
             for characteristic_tuple in itertools.product(*combined_char_indices_list):
+
+                # or logic
+                or_tuple = (characteristic_tuple[0], characteristic_tuple[1], characteristic_tuple[2])
+                or_characterstic_tuple = (or_tuple, orf_flag)
+
+                if (or_tuple[0] in combined_char_indices_list[0] or or_tuple[1] in combined_char_indices_list[1] or or_tuple[2] in combined_char_indices_list[2]):
+                    if or_characterstic_tuple in hypothesis_dict.keys():
+                        hypothesis_occurrence_count[or_characterstic_tuple] += 1
+                        hypothesis_dict[or_characterstic_tuple] += ((weighted_property_dict[or_tuple[0]]+weighted_property_dict[or_tuple[1]] + weighted_property_dict[or_tuple[2]])/2)*hypothesis_occurrence_count[or_tuple]
+                    else:
+                        hypothesis_occurrence_count[or_characterstic_tuple] = 1
+                        hypothesis_dict[or_characterstic_tuple] = (weighted_property_dict[or_tuple[0]]+weighted_property_dict[or_tuple[1]]+weighted_property_dict[or_tuple[2]])/2
+                mean += (weighted_property_dict[or_tuple[0]]+weighted_property_dict[or_tuple[1]]+weighted_property_dict[or_tuple[2]])/3
+                # and logic
                 if characteristic_tuple in hypothesis_dict.keys():
                     hypothesis_occurrence_count[characteristic_tuple] += 1
-                    hypothesis_dict[characteristic_tuple] += (weighted_property_dict[characteristic_tuple[0]] + weighted_property_dict[characteristic_tuple[1]] + weighted_property_dict[characteristic_tuple[2]]) / 3 * hypothesis_occurrence_count[characteristic_tuple]
+                    hypothesis_dict[characteristic_tuple] += ((weighted_property_dict[characteristic_tuple[0]]+weighted_property_dict[characteristic_tuple[1]]+weighted_property_dict[characteristic_tuple[2]])/3)*hypothesis_occurrence_count[characteristic_tuple]
                 else:
-                    hypothesis_dict[characteristic_tuple] = (weighted_property_dict[characteristic_tuple[0]] + weighted_property_dict[characteristic_tuple[1]] + weighted_property_dict[characteristic_tuple[2]]) / 3
+                    hypothesis_dict[characteristic_tuple] = (weighted_property_dict[characteristic_tuple[0]]+weighted_property_dict[characteristic_tuple[1]]+weighted_property_dict[characteristic_tuple[2]])/3
                     hypothesis_occurrence_count[characteristic_tuple] = 1
-                mean += (weighted_property_dict[characteristic_tuple[0]] + weighted_property_dict[characteristic_tuple[1]] + weighted_property_dict[characteristic_tuple[2]]) / 3
+                mean += (weighted_property_dict[characteristic_tuple[0]]+weighted_property_dict[characteristic_tuple[1]]+weighted_property_dict[characteristic_tuple[2]])/3
 
+        
     hypothesis_occurrence_count = {}
+
     for i in xrange(1, len(legal_cards)):
-        combined_char_indices_list = [char_dict[i - 1], char_dict[i]]
+        #Now we have individual chars in characteristic_index_list[i]
+        #Decide on how to formulate hypothesis
+        combined_char_indices_list = [char_dict[i-1], char_dict[i]]
+
         for characteristic_tuple in itertools.product(*combined_char_indices_list):
+
+            # or logic
+            or_tuple = (characteristic_tuple[0], characteristic_tuple[1])
+            or_characterstic_tuple = (or_tuple, orf_flag)
+
+            if (or_tuple[0] in combined_char_indices_list[0] or or_tuple[1] in combined_char_indices_list[1]):
+                if or_characterstic_tuple in hypothesis_dict.keys():
+                    hypothesis_occurrence_count[or_characterstic_tuple] += 1
+                    hypothesis_dict[or_characterstic_tuple] += ((weighted_property_dict[or_tuple[0]]+weighted_property_dict[or_tuple[1]])/2)*hypothesis_occurrence_count[or_tuple]
+                else:
+                    hypothesis_occurrence_count[or_characterstic_tuple] = 1
+                    hypothesis_dict[or_characterstic_tuple] = (weighted_property_dict[or_tuple[0]]+weighted_property_dict[or_tuple[1]])/2
+            mean += (weighted_property_dict[or_tuple[0]]+weighted_property_dict[or_tuple[1]])/3
+            # and logic
             if characteristic_tuple in hypothesis_dict.keys():
                 hypothesis_occurrence_count[characteristic_tuple] += 1
-                hypothesis_dict[characteristic_tuple] += (weighted_property_dict[characteristic_tuple[0]] + weighted_property_dict[characteristic_tuple[1]]) / 2 * hypothesis_occurrence_count[characteristic_tuple]
+                hypothesis_dict[characteristic_tuple] += ((weighted_property_dict[characteristic_tuple[0]]+weighted_property_dict[characteristic_tuple[1]])/2)*hypothesis_occurrence_count[characteristic_tuple]
             else:
                 hypothesis_occurrence_count[characteristic_tuple] = 1
-                hypothesis_dict[characteristic_tuple] = (weighted_property_dict[characteristic_tuple[0]] + weighted_property_dict[characteristic_tuple[1]]) / 2
-            mean += (weighted_property_dict[characteristic_tuple[0]] + weighted_property_dict[characteristic_tuple[1]]) / 2
+                hypothesis_dict[characteristic_tuple] = (weighted_property_dict[characteristic_tuple[0]]+weighted_property_dict[characteristic_tuple[1]])/2
 
-    ranked_hypothesis_dict = OrderedDict(sorted(hypothesis_dict.items(), key=lambda (key, value): (value, key), reverse=True))
+            mean += (weighted_property_dict[characteristic_tuple[0]]+weighted_property_dict[characteristic_tuple[1]])/2
+        
+    ranked_hypothesis_dict = OrderedDict(sorted(hypothesis_dict.items(), key = lambda (key, value) : (value, key), reverse=True))
+        
     hypothesis_offset = len(ranked_hypothesis_dict)
-    mean_cutoff = mean / hypothesis_offset
-    ranked_hypothesis = {}
+    if hypothesis_offset == 0:
+        hypothesis_offset = 1 
+    print('-------------------hypothesis_offset-----------------------', hypothesis_offset)
+    mean_cutoff = mean/hypothesis_offset
+    ranked_hypothesis = {} 
     for value in ranked_hypothesis_dict.iteritems():
-        if value[1] > mean_cutoff:
+        if value[1] > mean_cutoff :
             ranked_hypothesis[value[0]] = value[1]
         else:
             break
 
+    
+
+    #TODO eliminate conflicting hypothesis- ex: consecutive Royal/B/Odd & Non-Royal/R/Even. Check if already handled by pick_negative
+    #Using illegal cards to eliminate possible hypothesis
     illegal_tuple_list = parse_illegal_indices()
+    
     for elem in illegal_tuple_list:
         if len(elem) > 2 and three_length_hypothesis_flag:
             prev2 = elem[0]
             prev = elem[1]
             curr = elem[2]
+
             combined_char_indices_list = [get_card_mapping_characterstic(prev2), get_card_mapping_characterstic(prev), get_card_mapping_characterstic(curr)]
+
             hypothesis_occurrence_count = {}
+            
             for characteristic_tuple in itertools.product(*combined_char_indices_list):
-                if len(characteristic_tuple) > 2:
+                or_tuple = (characteristic_tuple[0], characteristic_tuple[1])
+                or_characterstic_tuple = (or_tuple, orf_flag)
+
+                if (or_tuple[0] not in combined_char_indices_list[0] and or_tuple[1] not in combined_char_indices_list[1] and or_tuple[2] not in combined_char_indices_list[2]):
+                        if or_characterstic_tuple in ranked_hypothesis.keys():
+                            if or_characterstic_tuple in hypothesis_occurrence_count:
+                                hypothesis_occurrence_count[or_characterstic_tuple] += 1
+                            else:
+                                hypothesis_occurrence_count[or_characterstic_tuple] = 1
+                            ranked_hypothesis[or_characterstic_tuple] -= ((weighted_property_dict[or_tuple[0]]+weighted_property_dict[or_tuple[1]] + weighted_property_dict[or_tuple[2]])/3)*hypothesis_occurrence_count[or_characterstic_tuple]
+
+                if(len(characteristic_tuple)>2):
                     if characteristic_tuple in ranked_hypothesis.keys():
                         if characteristic_tuple in hypothesis_occurrence_count:
                             hypothesis_occurrence_count[characteristic_tuple] += 1
                         else:
                             hypothesis_occurrence_count[characteristic_tuple] = 1
-                        ranked_hypothesis[characteristic_tuple] -= (weighted_property_dict[characteristic_tuple[0]] + weighted_property_dict[characteristic_tuple[1]] + weighted_property_dict[characteristic_tuple[2]]) / 3 * hypothesis_occurrence_count[characteristic_tuple]
+                        ranked_hypothesis[characteristic_tuple] -= ((weighted_property_dict[characteristic_tuple[0]]+weighted_property_dict[characteristic_tuple[1]]+weighted_property_dict[characteristic_tuple[2]])/3)*hypothesis_occurrence_count[characteristic_tuple]
 
         else:
             prev = elem[0]
             curr = elem[1]
+
             hypothesis_occurrence_count = {}
             combined_char_indices_list = [get_card_mapping_characterstic(prev), get_card_mapping_characterstic(curr)]
             for characteristic_tuple in itertools.product(*combined_char_indices_list):
-                if len(characteristic_tuple) > 1 and len(characteristic_tuple) <= 2:
+                or_tuple = (characteristic_tuple[0], characteristic_tuple[1])
+                or_characterstic_tuple = (or_tuple, orf_flag)
+                
+                # or logic for illegal cards
+                if ((len(or_characterstic_tuple)>1) and (len(or_characterstic_tuple)<=2)):
+                    if (or_tuple[0] not in combined_char_indices_list[0] and or_tuple[1] not in combined_char_indices_list[1]):
+                        if or_characterstic_tuple in ranked_hypothesis.keys():
+                            if or_characterstic_tuple in hypothesis_occurrence_count:
+                                hypothesis_occurrence_count[or_characterstic_tuple] += 1
+                            else:
+                                hypothesis_occurrence_count[or_characterstic_tuple] = 1
+                            ranked_hypothesis[or_characterstic_tuple] -= ((weighted_property_dict[or_tuple[0]]+weighted_property_dict[or_tuple[1]])/2)*hypothesis_occurrence_count[or_characterstic_tuple]
+
+                    # and logic
+                if ((len(characteristic_tuple)>1) and (len(characteristic_tuple)<=2)):
                     if characteristic_tuple in ranked_hypothesis.keys():
                         if characteristic_tuple in hypothesis_occurrence_count:
                             hypothesis_occurrence_count[characteristic_tuple] += 1
                         else:
                             hypothesis_occurrence_count[characteristic_tuple] = 1
-                        ranked_hypothesis[characteristic_tuple] -= (weighted_property_dict[characteristic_tuple[0]] + weighted_property_dict[characteristic_tuple[1]]) / 2 * hypothesis_occurrence_count[characteristic_tuple]
+                        ranked_hypothesis[characteristic_tuple] -= ((weighted_property_dict[characteristic_tuple[0]]+weighted_property_dict[characteristic_tuple[1]])/2)*hypothesis_occurrence_count[characteristic_tuple]
 
-    ranked_hypothesis = OrderedDict(sorted(ranked_hypothesis.items(), key=lambda (key, value): (value, key), reverse=True))
+    ranked_hypothesis = OrderedDict(sorted(ranked_hypothesis.items(), key = lambda (key, value) : (value, key), reverse=True))
+    
     return ranked_hypothesis
 
 
-def scan_and_rank_rules(ranked_hypothesis, numeric_hypothesis):
+def scan_and_rank_rules(ranked_hypothesis, numeric_hypothesis = []):
     board_state = parse_board_state()
     weighted_property_dict = set_characteristic_weights()
     legal_cards = board_state['legal_cards']
@@ -228,7 +317,6 @@ def scan_and_rank_rules(ranked_hypothesis, numeric_hypothesis):
     print str(pruned_ranked_hypothesis_index)
     pr_ranked_hypothesis = OrderedDict(sorted(pruned_ranked_hypothesis_dict.items(), key=lambda (key, value): (value, key), reverse=True))
     return pr_ranked_hypothesis
-
 
 def scan_and_rank_numeric_hypothesis(three_length_hypothesis_flag):
     hypothesis_dict = {}
